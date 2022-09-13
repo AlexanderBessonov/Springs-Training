8a77e1a11b1d81e8def7eb304de8e0145e6216c6
Alexander Bessonov <bessonovUSA@gmail.com>
2022-09-12 23:07:14 -0500

    Wiring

--- spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
+++ spring-03-Wiring/src/main/java/com/cydeo/casefactory/Case.java
@@ -11,10 +11,19 @@ public abstract class Case {
     private String manufacturer;
     private String powerSupply;
 
+    private Dimensions dimensions;
+
     public Case(String model, String manufacturer, String powerSupply) {
         this.model = model;
         this.manufacturer = manufacturer;
         this.powerSupply = powerSupply;
+    }
+
+    public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimensions = dimensions;
     }
 
     public abstract void pressPowerButton();
