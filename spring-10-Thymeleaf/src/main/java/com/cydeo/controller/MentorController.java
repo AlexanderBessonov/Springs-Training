5d99b65284336590764f9127ae2a32e2b0ddc237
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-21 13:38:24 -0500

    started  Model 11

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -33,8 +33,8 @@ public class MentorController {//locathost:8080/mentor/register
 
 //        model.addAttribute("mentor", new Mentor());
 //         return "mentor/mentor-register";
+        return "redirect:/mentor/register";
 
-         return "redirect: /mentor/register";
     }
 
     //*******************************************
