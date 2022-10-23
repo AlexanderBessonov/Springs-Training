c8c0d58884adc9f456dc83b03d8f505bcebee1dc
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-23 18:13:52 -0500

    Car

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Account.java
@@ -1,12 +1,12 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
-@Entity
+@MappedSuperclass
 public class Account {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private BigDecimal balance;
