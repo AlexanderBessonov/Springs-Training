a83aee00842dc5b796507560ee94bf65ce99cd21
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-24 18:41:57 -0500

    MerchantRepository

--- spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.entity.Merchant;
 import com.cydeo.entity.Payment;
 import com.cydeo.entity.PaymentDetail;
 import com.cydeo.enums.Status;
@@ -26,13 +27,17 @@ public class DataGenerator implements CommandLineRunner {
         PaymentDetail paymentDetail1 = new PaymentDetail(new BigDecimal("140000"),new BigDecimal("10000"),
                 LocalDate.of(2022,4,24));
 
-        payment1.setPaymentDetail(paymentDetail1);
 
         Payment payment2 = new Payment(LocalDate.of(2022,4,25),new BigDecimal("100000"), Status.FAILURE);
         PaymentDetail paymentDetail2 = new PaymentDetail(new BigDecimal("90000"),new BigDecimal("5000"),
                 LocalDate.of(2022,4,29));
 
+        payment1.setPaymentDetail(paymentDetail1);
         payment2.setPaymentDetail(paymentDetail2);
+
+        Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
+        payment1.setMerchant(merchant1);
+       payment2.setMerchant(merchant1);
 
        //paymentDetailRepository.save(paymentDetail1);//----
        // paymentDetailRepository.save(paymentDetail2);//----
