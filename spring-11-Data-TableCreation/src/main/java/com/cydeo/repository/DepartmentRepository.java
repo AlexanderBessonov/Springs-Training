89be0c7c379af9edc902ab8d0287520b972cdb4e
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-23 21:48:30 -0500

    added  employeeList, departmentList

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -1,10 +1,10 @@
 package com.cydeo.repository;
 
-import com.cydeo.entity.Car;
+import com.cydeo.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
-//@Repository
-public interface CarRepository extends JpaRepository<Car,Long> {
+public interface DepartmentRepository extends JpaRepository<Department,Integer> {
 
 }
--- spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -1,10 +1,9 @@
 package com.cydeo.repository;
 
-import com.cydeo.entity.Car;
+import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-@Repository
-//@Repository
-public interface CarRepository extends JpaRepository<Car,Long> {
 
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
