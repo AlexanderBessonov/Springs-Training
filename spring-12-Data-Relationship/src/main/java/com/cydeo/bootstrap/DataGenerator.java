8936c96c3291e4d05d35ccca2df71e65052d750b
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-24 20:46:08 -0500

    @ManyToMany

--- spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -4,10 +4,10 @@ import com.cydeo.entity.Merchant;
 import com.cydeo.entity.Payment;
 import com.cydeo.entity.PaymentDetail;
 import com.cydeo.enums.Status;
+import com.cydeo.repository.MerchantRepository;
 import com.cydeo.repository.PaymentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -16,9 +16,11 @@ import java.time.LocalDate;
 public class DataGenerator implements CommandLineRunner {
 
     private final PaymentRepository paymentRepository;
+    private final MerchantRepository merchantRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository) {
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository) {
         this.paymentRepository = paymentRepository;
+        this.merchantRepository = merchantRepository;
     }
 
     @Override
@@ -38,15 +40,15 @@ public class DataGenerator implements CommandLineRunner {
         Merchant merchant1 = new Merchant("AmazonSubMerchant","M123",new BigDecimal("0.25"),new BigDecimal("3.25"),5);
         payment1.setMerchant(merchant1);
        payment2.setMerchant(merchant1);
+       merchantRepository.save(merchant1);//#1 Ex Amazon
 
        //paymentDetailRepository.save(paymentDetail1);//----
        // paymentDetailRepository.save(paymentDetail2);//----
 
-        paymentRepository.save(payment1);
+        paymentRepository.save(payment1);//#2 Ex payments
         paymentRepository.save(payment2);
 
-        System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
-
-        paymentRepository.delete(payment1);
+      // System.out.println(paymentRepository.findById(2L).get().getPaymentDetail().getCommissionAmount());
+       // paymentRepository.delete(payment1);
     }
 }
--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Merchant.java
@@ -23,8 +23,8 @@ public class Merchant {
     private BigDecimal commissionRate;
     private Integer payoutDelayCount;
 
-//    @OneToMany(mappedBy = "merchant")// in OneToMany relationship, ownership belongs to many side
-//    private List<Payment> paymentList;//(mappedBy = "merchant")->don't try to create in "merchant"
+    @OneToMany(mappedBy = "merchant")// in OneToMany relationship, ownership belongs to many side
+    private List<Payment> paymentList;//(mappedBy = "merchant")->don't try to create in "merchant"
 
     public Merchant(String name, String code, BigDecimal transactionFee, BigDecimal commissionRate,
                     Integer payoutDelayCount) {
