2253f6abb1808090b53877acf2a4e8bca3162b1c
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-28 23:50:29 -0500

    +regionRepository.findByCountry("Canada")

--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Region.java
@@ -1,5 +1,18 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class Region {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "regions")
+@NoArgsConstructor
+@Data
+public class Region extends BaseEntity{
+
+    private String region;
+    private String country;
+
 }
