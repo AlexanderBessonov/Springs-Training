0837feff5541ca424a28f260418506f6f329c54f
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-29 02:22:24 -0500

    findByDivisionEndsWith findDistinctTop3ByDivisionContains

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,13 +11,17 @@ import org.springframework.stereotype.Component;
 public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("------------------REGIONS------------------------");
 
         System.out.println("findByCountry: "+regionRepository.findByCountry("Canada"));
 
@@ -29,6 +34,10 @@ public class QueryDemo implements CommandLineRunner {
 
         System.out.println("findTopByCountryContainsOrderByRegion: "+regionRepository.findTopByCountryContainsOrderByRegion("United States"));
 
+        System.out.println("------------------DEPARTMENTS------------------------");
 
+        System.out.println("findByDepartment"+departmentRepository.findByDepartment("Furniture"));
+
+        System.out.println("findByDivision"+departmentRepository.findByDivision("Health"));
     }
 }
