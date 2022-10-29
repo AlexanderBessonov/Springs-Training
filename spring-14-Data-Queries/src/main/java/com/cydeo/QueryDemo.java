fe9565700084cf00d20ddb7d2a84ec0c85c2427b
Alexander Bessonov <bessonovUSA@gmail.com>
2022-10-29 04:55:55 -0500

    @Query

--- spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/QueryDemo.java
@@ -1,6 +1,7 @@
 package com.cydeo;
 
 import com.cydeo.repository.DepartmentRepository;
+import com.cydeo.repository.EmployeeRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,10 +13,12 @@ public class QueryDemo implements CommandLineRunner {
 
     private final RegionRepository regionRepository;
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.regionRepository = regionRepository;
         this.departmentRepository = departmentRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
@@ -43,6 +46,10 @@ public class QueryDemo implements CommandLineRunner {
         System.out.println("findByDivisionEndsWith"+departmentRepository.findByDivisionEndsWith("ics"));
 
         System.out.println("findDistinctTop3ByDivisionContains"+departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+        System.out.println("------------------EMPLOYEES------------------------");
+
+        System.out.println("retrieveEmployeeDetail(): "+employeeRepository.retrieveEmployeeDetail());
 
     }
 }
--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
@@ -15,9 +17,33 @@ public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
     List <Employee> findByFirstNameAndLastNameOrEmail (String firstName, String lastName, String email);
 
     //Display all employees that first name is NOT
-    List <Employee> findByFirstNameNot (String email);
+    List <Employee> findByFirstNameIsNot (String email);
 
+    //Display all employees that last name starts with ""
+    List <Employee> findByLastNameStartingWith (String pattern);
 
+    //Display all employees with salaries higher than ""
+    List <Employee> findBySalaryGreaterThan (Integer salary);
 
+    //Display all employees with salaries less than ""
+    List <Employee> findBySalaryLessThan (Integer salary);
 
+    //Display all employees that has been hired between  "" and ""
+    List <Employee> findByHireDateBetween (LocalDate startDate, LocalDate endDate);
+
+    //Display all employees where salaries greater and equal to "" in order-salary
+    List <Employee> findBySalaryGreaterThanEqualOrderBySalary (Integer salary);
+
+    //Display top unique 3 employees than is making less than ""
+    List <Employee> findDistinctTop3BySalaryLessThan (Integer salary);
+
+    //Display all employees that who do NOT have email address
+    List <Employee> findByEmailIsNull (String email);
+
+    //************************************* JPQL or SQL*******************************************
+    @Query("SELECT employee FROM Employee employee WHERE employee.email='amcnee1@google.es'")
+    Employee retrieveEmployeeDetail();
+
+    @Query("SELECT e.salary from Employee  e WHERE e.email='amcnee1@google.es'")
+    Integer retrieveEmployeeSalary();
 }
