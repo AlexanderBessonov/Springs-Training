8e0e550690e90c3d4579e74d5e2913f6d22c4ee7
Alexander Bessonov <bessonovUSA@gmail.com>
2022-11-27 22:52:13 -0600

    getAllCourses(){

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -1,4 +1,33 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//@Controller
+@RestController
+@RequestMapping("/courses")
 public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+  //  @ResponseBody
+    public List<CourseDTO> getAllCourses(){
+
+        return courseService.getCourses();   // return All courses
+
+       // return "view";//----No
+    }
+
 }
