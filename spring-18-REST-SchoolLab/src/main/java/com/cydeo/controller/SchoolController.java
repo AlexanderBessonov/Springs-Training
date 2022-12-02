42595c79e153b16d7a6153e6af6876e4c094d8f8
Alexander Bessonov <bessonovUSA@gmail.com>
2022-12-02 12:57:37 -0600

    spring-18 REST SchoolLab updateAddress

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -9,9 +9,7 @@ import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,5 +56,10 @@ public class SchoolController {
          return  ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved", addressDTO));
     }
 
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") Long id, @RequestBody AddressDTO addressDTO) throws Exception {
 
+        addressDTO.setId(id);
+        addressService.update(addressDTO);
+    }
 }
